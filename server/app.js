3e8777e592366a24f84868adeddf3a2f93299d3e
Sydney M Loteria <sydneymloteria@Sydneys-MacBook-Pro-2.local>
2021-07-02 12:58:44 +0800

    Display nothing if no api key and secret

--- server/app.js
+++ server/app.js
@@ -22,6 +22,18 @@ const ignoreFavicon = (req, res, next) => {
   next();
 };
 
+const checkForApiKeyAndSecret = (req, res, next) => {
+  if (process.env.NODE_ENV === 'production') {
+    if (!req.headers['x-api-key'] && !req.headers['x-api-secret']) {
+      res.status(200).send('');
+    } else {
+      next();
+    }
+  } else {
+    next();
+  }
+};
+
 const app = express();
 
 app.use(cors());
@@ -57,7 +69,7 @@ app.get('/*.png', (req, res) => {
   res.sendFile(path.join(__dirname, '../dist', req.originalUrl.split('?')[0]));
 });
 
-app.get('*', (req, res) => {
+app.get('*', checkForApiKeyAndSecret, (req, res) => {
   const apiKey = req.headers['x-api-key'] || process.env.X_API_KEY;
   const apiSecret = req.headers['x-api-secret'] || process.env.X_API_SECRET;
   const { euid } = req.headers;
